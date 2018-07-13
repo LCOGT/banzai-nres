fdb2d6a38f3e71a822b483a0fe295e1f782c9892
Curtis McCully <cmccully@lco.global>
2018-07-13 21:14:06 +0000

    Added changelog and updated the version.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from setuptools import setup
 setup(name='banzai_nres',
       author=['Curtis McCully', 'G. Mirek Brandt', 'Timothy D. Brandt'],
       author_email=['cmccully@lco.global', 'gmbrandt@ucsb.edu', 'tbrandt@physics.ucsb.edu'],
-      version='0.0.1',
+      version='0.1.0',
       packages=['banzai_nres'],
       package_dir={'banzai_nres': 'banzai_nres'},
       setup_requires=['pytest-runner'],
