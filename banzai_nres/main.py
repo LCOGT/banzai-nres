ca54028f9fc34d4d00cb65d9411d372dbb0b1849
gmbrandt <gmbrandt@umail.ucsb.edu>
2018-07-18 16:29:18 -0700

    revert

--- banzai_nres/main.py
+++ banzai_nres/main.py
@@ -31,8 +31,6 @@ from banzai.images import Image
 
 from banzai.munge import munge
 
-from tests import test_e2e
-
 from banzai.utils import file_utils
 import banzai.tests.utils
 
@@ -44,15 +42,6 @@ ordered_stages = [bias.OverscanSubtractor,
                   trim.Trimmer,
                   bias.BiasSubtractor,
                   ]
-
-# begin functions which act as entry points
-def run_test_making_master_biases():
-    """
-    function that runs test_making_master_biases inside of test_e2e.py because
-    I cannot figure out how to make test_e2e.test_making_master_biases a console
-    entrypoint.
-    """
-    test_e2e.test_making_master_biases()
 
 
 # end of entry point functions
