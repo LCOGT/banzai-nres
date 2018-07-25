d3d6692c41d0c4cc0b7b0036d7fb61365fa13d78
gmbrandt <gmbrandt@umail.ucsb.edu>
2018-07-25 14:11:06 -0700

    tightening up, including images and image_utils

---
+++ banzai_nres/utils/image_utils.py
@@ -0,0 +1,47 @@
+from banzai import logs
+
+from banzai.utils.image_utils import get_bpm
+
+from banzai.munge import munge
+
+from banzai_nres.images import Image
+
+logger = logs.get_logger(__name__)
+
+def read_images(image_list, pipeline_context):
+    """
+    This is a copy of banzai.images.read_images
+    which will properly handle images which already have a Bad Pixel Mask (BPM)
+    as an extension in the fits file. Prior, if image.bpm existed, the main.run
+    program would output an empty list. All that has been added is
+
+            else:
+                images.append(image)
+
+    Parameters:
+        pipeline_context: Object which contains attributes which describe the database etc.
+        image_list: A list of path/filename to fits files.
+    Returns:
+        images: List of banzai.images.Image objects with attached bad pixel masks.
+    """
+
+    images = []
+    for filename in image_list:
+        try:
+            image = Image(pipeline_context, filename=filename)
+            munge(image, pipeline_context)
+            if image.bpm is None:
+                bpm = get_bpm(image, pipeline_context)
+                if bpm is None:
+                    logger.error('No BPM file exists for this image.',
+                                 extra={'tags': {'filename': image.filename}})
+                else:
+                    image.bpm = bpm
+                    images.append(image)
+            else:
+                images.append(image)
+        except Exception as e:
+            logger.error('Error loading {0}'.format(filename))
+            logger.error(e)
+            continue
+    return images
