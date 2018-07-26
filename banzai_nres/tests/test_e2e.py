c9be3f509b970bdabb1e407af468e9e9133cda76
gmbrandt <gmbrandt@umail.ucsb.edu>
2018-07-26 08:10:32 -0700

    removed attempt at easy test

--- banzai_nres/tests/test_e2e.py
+++ banzai_nres/tests/test_e2e.py
@@ -1,16 +1,6 @@
 import pytest
 
-from banzai import logs
-from banzai_nres.main import make_master_bias_console
-
-
-logger = logs.get_logger(__name__)
-
 
 @pytest.mark.e2e
 def test_e2e():
-    assert True
-
-def test_making_master_bias():
-    make_master_bias_console()
     assert True
