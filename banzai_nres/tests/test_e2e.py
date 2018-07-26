e4523162cb930e0b6c7367601bc70deaf9773b07
gmbrandt <gmbrandt@umail.ucsb.edu>
2018-07-26 12:08:28 -0700

    error if the bpm directory already exists, so now we delete the directory and then remake it.

--- banzai_nres/tests/test_e2e.py
+++ banzai_nres/tests/test_e2e.py
@@ -2,6 +2,7 @@ import pytest
 from banzai.dbs import create_db, populate_bpm_table
 import os
 import numpy as np
+import shutil
 from astropy.io import fits
 from banzai_nres.main import make_master_bias_console, make_master_dark_console, TestContext
 
@@ -10,15 +11,25 @@ def setup_module(module):
     create_db('./', db_address=os.environ['DB_URL'],
               configdb_address='http://configdbdev.lco.gtn/sites/')
 
+    # clearing the bad pixel mask folder if it exists
+    if os.path.exists('/archive/engineering/lsc/nres01/bpm'):
+        shutil.rmtree('/archive/engineering/lsc/nres01/bpm')
+    if os.path.exists('/archive/engineering/elp/nres02/bpm'):
+        shutil.rmtree('/archive/engineering/elp/nres02/bpm')
+
+    # building bpm folder
     os.makedirs('/archive/engineering/lsc/nres01/bpm')
     os.makedirs('/archive/engineering/elp/nres02/bpm')
 
+    # adding the bpm folder to database
     populate_bpm_table('/archive/engineering/lsc/nres01/bpm', db_address=os.environ['DB_URL'])
     populate_bpm_table('/archive/engineering/elp/nres02/bpm', db_address=os.environ['DB_URL'])
 
+    # using a bias fits as a template for the bpm fits.
     fits_file_to_copy = '/archive/engineering/lsc/nres01/20180328/raw/lscnrs01-fl09-20180328-0066-b00.fits.fz'
     date_marker = '20180725'
 
+    # generating the zeros bpm.
     with fits.open(fits_file_to_copy) as hdu_list:
         hdu_list[1].data = np.zeros(hdu_list[1].data.shape, dtype=np.uint8)
         hdu_list[1].header['OBSTYPE'] = 'BPM'
