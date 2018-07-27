e649bccd568ea5c4635521b2a23df384db1ba12d
gmbrandt <gmbrandt@umail.ucsb.edu>
2018-07-27 13:21:40 -0700

    funpack bug. We now funpack a file, copy it, edit headers, save it as fits, then fpack it.

--- banzai_nres/tests/test_e2e.py
+++ banzai_nres/tests/test_e2e.py
@@ -7,6 +7,7 @@ from astropy.io import fits
 from banzai_nres.main import make_master_bias_console, make_master_dark_console, TestContext
 
 
+
 def setup_module(module):
     create_db('./', db_address=os.environ['DB_URL'],
               configdb_address=os.environ['CONFIG_DB_URL'])
@@ -22,24 +23,35 @@ def setup_module(module):
     os.makedirs('/archive/engineering/elp/nres02/bpm')
 
     # using an arbitrary fits as a template for the bpm fits.
-    fits_file_to_copy = '/archive/engineering/lsc/nres01/20180228/raw/lscnrs01-fl09-20180228-0010-e00.fits.fz'
+    fits_file_to_copy = '/archive/engineering/lsc/nres01/20180228/raw/lscnrs01-fl09-20180228-0010-e00.fits'
     date_marker = '20180727'
 
+    os.system('funpack {file}'.format(file=fits_file_to_copy + '.fz'))
     # generating the zeros bpm. Files need to start with bpm.
     with fits.open(fits_file_to_copy) as hdu_list:
         hdu_list[1].data = np.zeros(hdu_list[1].data.shape, dtype=np.uint8)
         hdu_list[1].header['OBSTYPE'] = 'BPM'
         hdu_list[1].header['EXTNAME'] = 'BPM'
         hdu_list[1].header['INSTRUME'] = 'nres01'
-        hdu_list.writeto('/archive/engineering/lsc/nres01/bpm/bpm_lsc_fl09_' +
-                         date_marker + '.fits.fz', overwrite=True)
+        output_filename = '/archive/engineering/lsc/nres01/bpm/bpm_lsc_fl09_' \
+                          + date_marker + '.fits'
+        hdu_list.writeto(output_filename, overwrite=True)
+        # fpack the file (which creates an fpacked copy)
+        os.system('fpack {file}'.format(file=output_filename))
+        # delete the un-fpacked file.
+        os.system('rm {file}'.format(file=output_filename))
 
         hdu_list[1].header['INSTRUME'] = 'nres02'
         hdu_list[1].header['SITEID'] = 'elp'
         hdu_list[1].header['TELESCOP'] = 'nres02'
-        hdu_list.writeto('/archive/engineering/elp/nres02/bpm/bpm_elp_fl17_'
-                         + date_marker + '.fits.fz', overwrite=True)
+        output_filename = '/archive/engineering/elp/nres02/bpm/bpm_elp_fl17_' \
+                          + date_marker + '.fits'
+        hdu_list.writeto(output_filename, overwrite=True)
+        os.system('fpack {file}'.format(file=output_filename))
+        os.system('rm {file}'.format(file=output_filename))
 
+    # delete the unpacked file which was initially copied.
+    os.system('rm {file}'.format(file=fits_file_to_copy))
     # adding the bpm folder to database and populating the sqlite tables.
     populate_bpm_table('/archive/engineering/lsc/nres01/bpm', db_address=os.environ['DB_URL'])
     populate_bpm_table('/archive/engineering/elp/nres02/bpm', db_address=os.environ['DB_URL'])
