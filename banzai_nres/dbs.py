54bb12341a22ad0efca86b4f78a7a52264ec4176
gmbrandt <gmbrandt@umail.ucsb.edu>
2018-08-06 11:31:34 -0700

    would crash if loading a LAMPFLAT while there did not exist a pre-existing master trace

--- banzai_nres/dbs.py
+++ banzai_nres/dbs.py
@@ -5,17 +5,16 @@ import os
 
 def get_trace_coefficients(image):
     coefficients_and_indices, fiber_order = None, None
-    if image.header['OBSTYPE'] != 'TRACE':
-        master_trace_filename = CalibrationMaker.get_calibration_filename(CalibrationMaker(image.pipeline_context), image)
-        master_trace_file_path = os.path.join(image.pipeline_context.processed_path, master_trace_filename)
-
-        if not os.path.isfile(master_trace_file_path):
-            raise MasterCalibrationDoesNotExist
-
+    master_trace_filename = CalibrationMaker.get_calibration_filename(CalibrationMaker(image.pipeline_context), image)
+    master_trace_file_path = os.path.join(image.pipeline_context.processed_path, master_trace_filename)
+    if image.header['OBSTYPE'] != 'TRACE' and os.path.isfile(master_trace_file_path):
         fiber_order = fits.getheader(master_trace_file_path).get('FIBRORDR')
         coefficients_and_indices = fits.getdata(master_trace_file_path)
 
         assert coefficients_and_indices is not None
         assert fiber_order is not None
+
+    if image.header['OBSTYPE'] != 'LAMPFLAT' and not os.path.isfile(master_trace_file_path):
+        raise MasterCalibrationDoesNotExist
 
     return coefficients_and_indices, fiber_order
--- banzai_nres/main.py
+++ banzai_nres/main.py
@@ -28,7 +28,8 @@ banzai_main.ordered_stages = [qc.HeaderSanity,
                               gain.GainNormalizer,
                               trim.Trimmer,
                               bias.BiasSubtractor,
-                              dark.DarkSubtractor]
+                              dark.DarkSubtractor,
+                              traces.TraceUpdater]
 
 
 def make_master_bias_console():
@@ -60,12 +61,12 @@ def make_master_dark(pipeline_context):
 
 
 def make_master_trace(pipeline_context):
-    stages_to_do = get_stages_todo(dark.DarkSubtractor, extra_stages=[traces.TraceFitOrderbyOrder])
+    stages_to_do = get_stages_todo(dark.DarkSubtractor, extra_stages=[traces.TraceMaker])
     run(stages_to_do, pipeline_context, image_types=['LAMPFLAT'], calibration_maker=True,
         log_message='Making Master Trace by Updating Previous Master with global-meta Technique')
 
 
 def make_master_trace_blind(pipeline_context):
-    stages_to_do = get_stages_todo(dark.DarkSubtractor, extra_stages=[traces.TraceFitOrderbyOrder])
+    stages_to_do = get_stages_todo(dark.DarkSubtractor, extra_stages=[traces.BlindTraceMaker])
     run(stages_to_do, pipeline_context, image_types=['LAMPFLAT'], calibration_maker=True,
         log_message='Making Master Trace via order-by-order Blind Technique!')
