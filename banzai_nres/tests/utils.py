f91e3fe4bddef10d53c43bbe68b76e1ad3b51c75
gmbrandt <gmbrandt@umail.ucsb.edu>
2018-08-07 10:49:56 -0700

    error in test utils

--- banzai_nres/tests/utils.py
+++ banzai_nres/tests/utils.py
@@ -40,12 +40,15 @@ class FakeTraceImage(FakeImage):
 
 
 def trim_coefficients_to_fit_image(image, trace_fit_coefficients_no_indices):
+    min_y, max_y = 0, image.data.shape[0]
     order_indices = np.array([i for i in range(0, trace_fit_coefficients_no_indices.shape[0])])
     trace_fit_coefficients = np.insert(trace_fit_coefficients_no_indices, obj=0, values=order_indices, axis=1)
     trace_values_versus_xpixel, num_traces, x = get_trace_centroids_from_coefficients(trace_fit_coefficients, image)
+    good_indices = []
     for i in range(trace_values_versus_xpixel.shape[0]):
+        if (trace_values_versus_xpixel[i,:] < max_y).all() and (trace_values_versus_xpixel[i,:] > min_y).all():
+            good_indices += [i]
 
-    return 0
 
 def random_yet_realistic_trace_coefficients(image):
     """
