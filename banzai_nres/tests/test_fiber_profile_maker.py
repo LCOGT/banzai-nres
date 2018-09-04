7a89dc85b7ec19352304995d380087309d496c80
gmbrandt <gmbrandt@umail.ucsb.edu>
2018-09-04 15:32:10 -0700

    fixed missing elements for testing fiber_profile_maker

--- banzai_nres/tests/test_fiber_profile_maker.py
+++ banzai_nres/tests/test_fiber_profile_maker.py
@@ -17,21 +17,34 @@ from banzai_nres.fiber_profile import SampleFiberProfileAcrossImage, GenerateFib
 logger = logs.get_logger(__name__)
 
 
-def _generate_image_with_two_flat_traces(readnoise=10, order_width=1.25):
-    image = FakeImage(nx=1000, ny=102)
+def generate_image_with_two_flat_traces(readnoise=10, order_width=1.25):
+    nx = 1000
+    ny = 100
+    image = FakeImage(nx=nx, ny=(ny+2))
     trace_coefficients_no_indices = np.array([[image.data.shape[0]*1/3, 0, 0],
                                               [image.data.shape[0]*2/3, 0, 0]])
+
     image.trace.coefficients = trim_coefficients_to_fit_image(image, trace_coefficients_no_indices)
     fill_image_with_traces(image, order_width=order_width)
     image.readnoise = readnoise
-    noisify_image(image, trimmed_shape=(100, 1000))
-    trim_image(image, trimmed_shape=(100, 1000))
+    noisify_image(image, trimmed_shape=(ny, nx))
+    trim_image(image, trimmed_shape=(ny, nx))
     return image
+
+
+def append_good_region_info(image):
+    image.trace.has_sufficient_signal_to_noise = np.ones(len(image.trace.coefficients)).astype(bool)
+    image.trace.high_signal_to_noise_region_bounds = np.ones((len(image.trace.coefficients), 2))
+    image.trace.high_signal_to_noise_region_bounds[:, 0] = 0
+    image.trace.high_signal_to_noise_region_bounds[:, 1] = image.data.shape[1]-1
 
 
 def test_fiber_profile_maker():
     real_full_width_half_max = 1.25
-    images = [_generate_image_with_two_flat_traces(order_width=real_full_width_half_max)]
+    image = generate_image_with_two_flat_traces(order_width=real_full_width_half_max)
+    append_good_region_info(image)
+    images = [image]
+
     sampling_stage = SampleFiberProfileAcrossImage(FakeContext())
     sampling_stage.do_stage(images)
     fiber_profile_maker_stage = GenerateFiberProfileImage(FakeContext())
