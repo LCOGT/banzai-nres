7debea473c136b7de09394394ca771b4ba65b30a
gmbrandt <gmbrandt@umail.ucsb.edu>
2018-09-17 17:59:43 -0700

    fixed changes from master

--- banzai_nres/main.py
+++ banzai_nres/main.py
@@ -10,6 +10,7 @@ July 2018
 
 from banzai_nres.bias import BiasMaker as nres_BiasMaker
 from banzai_nres.dark import DarkMaker as nres_DarkMaker
+from banzai_nres import traces
 
 from banzai import bias, trim, dark, gain
 from banzai import logs, qc
@@ -30,7 +31,8 @@ banzai_main.ORDERED_STAGES = [qc.HeaderSanity,
                               gain.GainNormalizer,
                               trim.Trimmer,
                               bias.BiasSubtractor,
-                              dark.DarkSubtractor]
+                              dark.DarkSubtractor,
+                              traces.LoadInitialGuessForTraceFit]
 
 
 def make_master_bias_console():
@@ -44,6 +46,14 @@ def make_master_dark_console():
     run_end_of_night_from_console([make_master_dark], NRES_CRITERIA)
 
 
+def make_master_trace_console():
+    run_end_of_night_from_console([make_master_trace], NRES_CRITERIA)
+
+
+def make_master_trace_blind_console():
+    run_end_of_night_from_console([make_master_trace_blind], NRES_CRITERIA)
+
+
 def make_master_bias(pipeline_context):
     stages_to_do = get_stages_todo(trim.Trimmer, extra_stages=[bias.BiasMasterLevelSubtractor, nres_BiasMaker])
     run(stages_to_do, pipeline_context, image_types=['BIAS'], calibration_maker=True,
@@ -54,3 +64,18 @@ def make_master_dark(pipeline_context):
     stages_to_do = get_stages_todo(bias.BiasSubtractor, extra_stages=[dark.DarkNormalizer, nres_DarkMaker])
     run(stages_to_do, pipeline_context, image_types=['DARK'], calibration_maker=True,
         log_message='Making Master Dark')
+
+
+def make_master_trace(pipeline_context):
+    stages_to_do = get_stages_todo(traces.LoadInitialGuessForTraceFit, extra_stages=[traces.TraceRefine,
+                                                                                     traces.TraceSaver])
+    run(stages_to_do, pipeline_context, image_types=['LAMPFLAT'], calibration_maker=True,
+        log_message='Making Master Trace by Updating Previous Master with global-meta Technique')
+
+
+def make_master_trace_blind(pipeline_context):
+    stages_to_do = get_stages_todo(dark.DarkSubtractor, extra_stages=[traces.GenerateInitialGuessForTraceFitFromScratch,
+                                                                      traces.TraceRefine,
+                                                                      traces.TraceSaver])
+    run(stages_to_do, pipeline_context, image_types=['LAMPFLAT'], calibration_maker=True,
+        log_message='Making Master Trace via order-by-order Blind Technique!')
