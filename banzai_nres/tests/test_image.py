e42f480236244cc7e10d9119ecec146562fa36ab
gmbrandt <gmbrandt@umail.ucsb.edu>
2018-09-20 13:06:55 -0700

    unit test for adding class

--- banzai_nres/tests/test_image.py
+++ banzai_nres/tests/test_image.py
@@ -1,7 +1,26 @@
-from banzai_nres import images
+from banzai_nres.images import Image
 from banzai.tests.utils import FakeContext
+from banzai_nres.utils.NRES_class_utils import add_class_as_attribute
+
+
+class AClass(object):
+    def __init__(self):
+        self.an_attribute = None
 
 
 def test_image_class_loads():
-    image = images.Image(pipeline_context=FakeContext())
+    image = Image(pipeline_context=FakeContext())
     assert image.trace is None
+
+
+def test_adding_class_as_attribute():
+    image = Image(pipeline_context=FakeContext())
+    setattr(image, 'a_class', None)
+    images = [image]
+    add_class_as_attribute(images, 'a_class', AClass)
+    assert images[0].a_class.an_attribute is None
+    
+    image = Image(pipeline_context=FakeContext())
+    images = [image]
+    add_class_as_attribute(images, 'a_class', AClass)
+    assert images[0].a_class.an_attribute is None
