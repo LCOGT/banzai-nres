22b02e3fa432e9ae5571d1b960358f7afe7372e0
Curtis McCully <cmccully@lco.global>
2018-10-26 16:12:35 +0000

    Fixed typo in argument.

--- banzai_nres/main.py
+++ banzai_nres/main.py
@@ -35,8 +35,7 @@ banzai_main.ORDERED_STAGES = [qc.HeaderSanity,
 
 
 def make_master_bias(pipeline_context=None, raw_path=None):
-    pipeline_context, raw_path = parse_directory_args(pipeline_context, raw_path, NRES_CRITERIA,
-                                                      parser_description='Reduce LCO NRES data.')
+    pipeline_context, raw_path = parse_directory_args(pipeline_context, raw_path, NRES_CRITERIA)
     process_directory(pipeline_context, raw_path, ['BIAS'], last_stage=trim.Trimmer,
                       extra_stages=[bias.BiasMasterLevelSubtractor, nres_BiasMaker],
                       log_message='Making Master BIAS', calibration_maker=True)
