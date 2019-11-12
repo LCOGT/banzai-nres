43f679db9fb7d9ef25884bf23cadabce68fbed3e
Curtis McCully <cmccully@lco.global>
2019-11-12 00:55:57 +0000

    Fixes to add flat stacking to automatic processing.

--- banzai_nres/settings.py
+++ banzai_nres/settings.py
@@ -25,31 +25,25 @@ CALIBRATION_FILENAME_FUNCTIONS = {'BIAS': ('banzai.utils.file_utils.config_to_fi
                                                     'banzai.utils.file_utils.ccdsum_to_filename'),
                                            'LAMPFLAT': ('banzai.utils.file_utils.config_to_filename',
                                                         'banzai.utils.file_utils.ccdsum_to_filename',
-                                                        'banzai_nres.fibers.fibers_state_to_filename'),
-                                           'TRACE': ('banzai.utils.file_utils.config_to_filename',
-                                                     'banzai.utils.file_utils.ccdsum_to_filename',
-                                                     'banzai_nres.fibers.fibers_state_to_filename')}
+                                                        'banzai_nres.fibers.fibers_state_to_filename')}
 
 TELESCOPE_FILENAME_FUNCTION = 'banzai_nres.utils.runtime_utils.get_telescope_filename'
 
 
-CALIBRATION_IMAGE_TYPES = ['BIAS', 'DARK', 'LAMPFLAT', 'TRACE']
+CALIBRATION_IMAGE_TYPES = ['BIAS', 'DARK', 'LAMPFLAT']
 
 LAST_STAGE = {'BIAS': 'banzai.trim.Trimmer',
                        'DARK': 'banzai.bias.BiasSubtractor',
-                       'LAMPFLAT': 'banzai.dark.DarkSubtractor',
-                       'TRACE': 'banzai.dark.DarkSubtractor'}
+                       'LAMPFLAT': 'banzai.dark.DarkSubtractor'}
 
 EXTRA_STAGES = {'BIAS': ['banzai.bias.BiasMasterLevelSubtractor', 'banzai.bias.BiasComparer'],
                          'DARK': ['banzai.dark.DarkNormalizer', 'banzai.dark.DarkComparer'],
-                         'LAMPFLAT': [],
-                         'TRACE': []}
+                         'LAMPFLAT': []}
 
 
 CALIBRATION_STACKER_STAGES = {'BIAS': ['banzai.bias.BiasMaker'],
                               'DARK': ['banzai.dark.DarkMaker'],
-                               'LAMPFLAT': [
-                                   'banzai_nres.flats.FlatStacker']}
+                               'LAMPFLAT': ['banzai_nres.flats.FlatStacker']}
 
 # Stack delays are expressed in seconds--namely, each is five minutes
 CALIBRATION_STACK_DELAYS = {'BIAS': 300,
@@ -57,12 +51,10 @@ CALIBRATION_STACK_DELAYS = {'BIAS': 300,
                             'LAMPFLAT': 300}
 
 
-SCHEDULE_STACKING_CRON_ENTRIES = {'coj': {'minute': 30, 'hour': 6},
-                                  'cpt': {'minute': 0, 'hour': 15},
-                                  'tfn': {'minute': 30, 'hour': 17},
+SCHEDULE_STACKING_CRON_ENTRIES = {'cpt': {'minute': 0, 'hour': 15},
+                                  'tlv': {'minute': 45, 'hour': 8},
                                   'lsc': {'minute': 0, 'hour': 21},
-                                  'elp': {'minute': 0, 'hour': 23},
-                                  'ogg': {'minute': 0, 'hour': 3}}
+                                  'elp': {'minute': 0, 'hour': 23}}
 
 OBSERVATION_PORTAL_URL = os.getenv('OBSERVATION_PORTAL_URL', 'http://internal-observation-portal.lco.gtn/api/observations/')
 CALIBRATE_PROPOSAL_ID = os.getenv('CALIBRATE_PROPOSAL_ID', 'calibrate')
