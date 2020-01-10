d5f4e5cffcc2ab8240d7dd41113ee77b5bb7c140
Curtis McCully <cmccully@lco.global>
2020-01-10 10:26:04 -0800

    Better behaved background subtraction

--- banzai_nres/background.py
+++ banzai_nres/background.py
@@ -1,23 +1,23 @@
 from banzai.stages import Stage
-from scipy.interpolate import LSQBivariateSpline
 import numpy as np
+from scipy.interpolate import SmoothBivariateSpline
 
 
 class BackgroundSubtractor(Stage):
     def do_stage(self, image):
-        # TODO: Add iteratively reweighted LSQ
-        indices_to_interpolate = np.logical_or(image.trace > 0, image.mask)
+
+        indices_to_interpolate = np.logical_or(image.traces > 0, image.mask)
+
         x1d, y1d = np.arange(image.shape[1]), np.arange(image.shape[0])
         X, Y = np.meshgrid(x1d, y1d)
-        weights = image.primary_hdu.uncertainty ** -2.0
-        estimator = LSQBivariateSpline(X[~indices_to_interpolate], Y[~indices_to_interpolate],
-                                       image.data[~indices_to_interpolate], w=weights[~indices_to_interpolate],
-                                       tx=np.arange(self.runtime_context.background_smoothing_scale, image.shape[1], 
-                                                    self.runtime_context.background_smoothing_scale),
-                                       ty=np.arange(self.runtime_context.background_smoothing_scale, image.shape[0], 
-                                                    self.runtime_context.background_smoothing_scale),
-                                                    kx=self.runtime_context.background_spline_order, 
-                                                    ky=self.runtime_context.background_spline_order)
-                                       
+
+        # Note that from the docs, the weights should be about 1 over the standard deviation for the smoothing to
+        # work well
+        # TODO: add reweighting
+        # because we can control the weights we can add iterative reweighting easily to do a better job of
+        # of rejecting outliers
+        weights = 1.0 / image.primary_hdu.uncertainty
+        estimator = SmoothBivariateSpline(X[~indices_to_interpolate], Y[~indices_to_interpolate],
+                                          image.data[~indices_to_interpolate], w=weights[~indices_to_interpolate])
         image.background = estimator(x1d, y1d).T
         return image
--- banzai_nres/tests/test_background_subtraction.py
+++ banzai_nres/tests/test_background_subtraction.py
@@ -3,6 +3,7 @@ from banzai.images import CCDData
 import numpy as np
 from banzai_nres.background import BackgroundSubtractor
 from banzai import context
+from scipy.stats import anderson
 
 
 def test_background_subtraction_on_noisy_data():
@@ -13,13 +14,18 @@ def test_background_subtraction_on_noisy_data():
     noise_sigma = 1.0
     input_background = 30 * np.exp(-(X - nx / 2.0)**2/10**2 - Y**2/45**2)
     test_data = input_background + np.random.normal(0.0, noise_sigma, size=input_background.shape)
-    test_image = NRESObservationFrame([CCDData(data=test_data, uncertainty=np.ones((ny, nx)) * 3.0,
+    test_image = NRESObservationFrame([CCDData(data=test_data, uncertainty=np.ones((ny, nx)) * noise_sigma,
                                       meta={'OBJECTS': 'tung&tung&none'})], 'foo.fits')
-    test_image.trace = np.zeros((ny, nx))
-    input_context = context.Context({'background_smoothing_scale': 15, 'background_spline_order': 2})
+    test_image.traces = np.zeros((ny, nx))
+    input_context = context.Context({'background_smoothing_scale': 30, 'background_spline_order': 1})
     stage = BackgroundSubtractor(input_context)
     output_image = stage.do_stage(test_image)
-    np.testing.assert_allclose(output_image.background.data, input_background, atol=3.0)
+    # Make sure our background estimation is good. This is roughly 1.5 sigma which is not bad
+    np.testing.assert_allclose(output_image.background.data, input_background, atol=1.5)
+    # Check that the remaining image basically looks like noise
+    difference = output_image.data - output_image.background
+    anderson_test = anderson(difference.ravel(), 'norm')
+    assert np.all(anderson_test.statistic < anderson_test.critical_values)
 
 
 def test_background_subtraction_with_traces():
@@ -30,12 +36,16 @@ def test_background_subtraction_with_traces():
     noise_sigma = 1.0
     input_background = 30 * np.exp(-(X - nx / 2.0)**2/10**2 - Y**2/45**2)
     test_data = input_background + np.random.normal(0.0, noise_sigma, size=input_background.shape)
-    test_image = NRESObservationFrame([CCDData(data=test_data, uncertainty=np.ones((ny, nx)) * 3.0,
+    test_image = NRESObservationFrame([CCDData(data=test_data, uncertainty=np.ones((ny, nx)) * noise_sigma,
                                       meta={'OBJECTS': 'tung&tung&none'})], 'foo.fits')
-    test_image.trace = np.zeros((ny, nx))
-    test_image.trace[20:30] = 1
+    test_image.traces = np.zeros((ny, nx))
+    test_image.traces[20:30] = 1
     input_context = context.Context({'background_smoothing_scale': 15, 'background_spline_order': 2})
     stage = BackgroundSubtractor(input_context)
     output_image = stage.do_stage(test_image)
-    np.testing.assert_allclose(output_image.background.data, input_background, atol=3.0)
-
+    # Make sure our background estimation is good. This is roughly 1.5 sigma which is not bad
+    np.testing.assert_allclose(output_image.background, input_background, atol=1.5)
+    # Check that the remaining image basically looks like noise
+    difference = output_image.data - output_image.background.data
+    anderson_test = anderson(difference.ravel(), 'norm')
+    assert np.all(anderson_test.statistic < anderson_test.critical_values)
