171092416fa20ae25a3ad6a32bee3f1eaf6cd31f
gmbrandt <gmbrandt@ucsb.edu>
2020-02-28 11:05:04 -0800

    changed itrace to xy.

--- banzai_nres/extract.py
+++ banzai_nres/extract.py
@@ -11,12 +11,12 @@ class SpectrumExtractor(Stage):
     def do_stage(self, image: EchelleSpectralCCDData):
         flux = np.zeros((image.num_traces, image.traces.shape[1]), dtype=float)
         variance = np.zeros_like(flux)
-        trace_ids, indexed_traces = index_traces(image.traces)
+        trace_ids, trace_xypos = index_traces(image.traces)
 
-        for i, itrace in enumerate(indexed_traces):
-            weights = self.weights(itrace, image)
-            flux[i] = self.extract_order(image.data[itrace], weights)
-            variance[i] = self.extract_order(image.uncertainty[itrace] ** 2, weights ** 2)
+        for i, xy in enumerate(trace_ids, trace_xypos):
+            weights = self.weights(xy, image)
+            flux[i] = self.extract_order(image.data[xy], weights)
+            variance[i] = self.extract_order(image.uncertainty[xy] ** 2, weights ** 2)
 
         image.spectrum = Table({'flux': flux, 'uncertainty': np.sqrt(variance), 'id': trace_ids})
         return image
@@ -26,38 +26,37 @@ class SpectrumExtractor(Stage):
         return np.sum(values * weights, axis=0)
 
     @abc.abstractmethod
-    def weights(self, itrace, image):
+    def weights(self, xy, image):
         """
-        :param itrace: ndarray. ndarray of indices such that image.data[itrace] returns
+        :param xy: ndarray. ndarray of indices such that image.data[xy] returns
         an ndarray of flux of shape N,M where M is e.g. 4096 (num of x pixels in the image data)
-        and N is the width of that trace, e.g. 20.
+        and N is the vertical height of that trace, e.g. 20.
         :param image:
-        :return: ndarray. Has same shape as itrace.
+        :return: ndarray. Has same shape as xy.
         """
-        return self._weights(itrace, image.profile, image.uncertainty**2, image.mask)
+        return self._weights(xy, image.profile, image.uncertainty**2, image.mask)
 
-    def _weights(self, itrace, profile_im, var_im, mask):
+    def _weights(self, xy, profile_im, var_im, mask):
         """
         Calculates the optimal extraction weights for a single order
         per Horne (1986, 1986PASP...98..609H) Equation 8 .
         The optimally extracted spectrum is then the sum of data * weights
         The associated variance is by definition var * weights**2 , which agrees with Horne (1986)
         equation 9.
-        :param itrace: ndarray. ndarray of indices such that image.data[itrace] returns
+        :param xy: ndarray. ndarray of indices such that image.data[xy] returns
         an ndarray of flux of shape N,M where M is e.g. 4096 (num of x pixels in the image data)
-        and N is the width of that trace, e.g. 20.
+        and N is the vertical height of that trace, e.g. 20.
         :param profile_im: ndarray. 2d image of the profile.
         :param var_im: ndarray. 2d image of the variance per pixel
         :param mask: ndarray. 2d image where 0 is a good pixel and 1 is a masked, bad pixel.
-        :return: ndarray. Has same shape as itrace.
+        :return: ndarray. Has same shape as xy.
         """
         if np.allclose(mask, 1):
-            return np.zeros_like(itrace)
-        invmask = np.invert(mask[itrace].astype(bool))  # array such that pixels to ignore have value 0
-        return invmask * profile_im[itrace] / var_im[itrace] / self.extract_order(1, invmask *
-                                                                                  profile_im[itrace] ** 2 / var_im[itrace])
+            return np.zeros_like(xy)
+        invmask = np.invert(mask[xy].astype(bool))  # array such that pixels to ignore have value 0
+        return invmask * profile_im[xy] / var_im[xy] / self.extract_order(1, invmask * profile_im[xy] ** 2 / var_im[xy])
 
 
 class BoxExtractor(SpectrumExtractor):
-    def weights(self, itrace, image):
-        return np.ones_like(itrace)
+    def weights(self, xy, image):
+        return np.ones_like(xy)
--- banzai_nres/utils/extract_utils.py
+++ banzai_nres/utils/extract_utils.py
@@ -4,7 +4,7 @@ import numpy as np
 def index_traces(traces):
     """
     :param traces: ndarray.
-           A 2d image where every pixel within a single trace is identified by one index, e.g. 1
+           A 2d image where every pixel within a single trace is identified by one index, e.g. 1,
            and every pixel in the background is 0.
            For example:
            trace = np.array([[0, 0, 0],
@@ -13,26 +13,27 @@ def index_traces(traces):
                              [2, 2, 2]])
            represents an image with two traces running left to right, labelled trace 1 and trace 2
            respectively.
-    :return: idx, trace_idxs: list, list
-            idx is a list of integers, labelling each entry in trace_idxs
-            trace_idxs is a list of ndarrays, call the ith one A_i. Each A_i is such that
+    :return: trace_ids, trace_xypos: list, list
+            trace_ids = [1,2,....,N] where N is the total number of traces in the image.
+            trace_xypos is a list of ndarrays, call the ith one A_i. Each A_i is such that
             data[A_i] will yield a spectrum of shape N_i, M where M is traces.shape[1]
             and N_i is the width of the trace in question.
     """
     if np.allclose(traces, 0):
-        # if there are no traces, return
+        # if there are no traces, return empty lists.
         return [], []
-    nx = traces.shape[1]
-    # TODO the below is a lazy and very slow version of turning the traces array into a set
-    #  of arrays A_i such that image.data[A_i] gives a properly flattened spectrum for the trace.
-    #  I or someone else will make something much faster later.
-    #  Also it does not work right now.
-    idx = []
-    trace_idxs = []
-    for i in range(1, int(np.max(traces)) + 1):
-        idx.append(i)
-        row, col = np.where(np.isclose(traces, i))
-        row, col = row.reshape(-1, nx), col.reshape(-1, nx)
-        trace_idxs.append([row, col])
+    trace_ids = np.arange(1, int(np.max(traces)) + 1)
+    trace_xypos = []
+    # TODO make this faster.
+    for trace_id in trace_ids:
+        y, x = np.where(np.isclose(traces, trace_id))
+        sorted_indices = np.lexsort((y, x), axis=0)
+        trace_width = int(max(x) - min(x)) + 1
+        xpos = x[sorted_indices].reshape(-1, trace_width)
+        ypos = y[sorted_indices].reshape(-1, trace_width)
 
-    return idx, trace_idxs
+        #xpos, ypos = x.reshape(-1, trace_width), y.reshape(-1, trace_width)
+
+        trace_xypos.append([xpos, ypos])
+
+    return trace_ids, trace_xypos
