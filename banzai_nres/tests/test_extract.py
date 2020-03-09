999a1540c1ee37709949fcd4ba1cbb9455a93b9b
Marshall Johnson <mjohnson@lco.global>
2020-03-09 13:32:02 -0700

    added more realistic test spectrum, and tests thereon, which are not yet working completely as intended

--- banzai_nres/tests/test_extract.py
+++ banzai_nres/tests/test_extract.py
@@ -38,6 +38,42 @@ class TestExtract:
 
         assert np.allclose(spectrum['pixel'], np.arange(image.data.shape[1]))
 
+    @pytest.mark.integration
+    def test_weights_in_poisson_regime(self):
+        trace_width=20
+        image = five_hundred_square_image(1000,10,trace_width)
+        image2 = NRESObservationFrame([EchelleSpectralCCDData(data=image.data, uncertainty=image.uncertainty,
+                                                         meta={'OBJECTS': 'tung&tung&none'})], 'foo.fits')
+        image2.traces = image.traces
+        input_context = context.Context({})
+
+        stage = GetOptimalExtractionWeights(input_context)
+        image = stage.do_stage(image)
+        image2.weights = np.ones_like(image2.data)/trace_width #correct for normalization
+        stage2 = WeightedExtract(input_context)
+        optimal_image = stage2.do_stage(image)
+        box_image = stage2.do_stage(image2)
+        assert np.allclose(optimal_image.spectrum['uncertainty'], box_image.spectrum['uncertainty'],0.1)
+
+    #note the following test does not yet work as intended
+    @pytest.mark.integration
+    def test_weights_in_readnoise_regime(self):
+        trace_width=20
+        image = five_hundred_square_image(1,10,trace_width)
+        image2 = NRESObservationFrame([EchelleSpectralCCDData(data=image.data, uncertainty=image.uncertainty,
+                                                         meta={'OBJECTS': 'tung&tung&none'})], 'foo.fits')
+        image2.traces = image.traces
+        input_context = context.Context({})
+
+        stage = GetOptimalExtractionWeights(input_context)
+        image = stage.do_stage(image)
+        image2.weights = np.ones_like(image2.data)/trace_width #correct for normalization
+        stage2 = WeightedExtract(input_context)
+        optimal_image = stage2.do_stage(image)
+        box_image = stage2.do_stage(image2)
+        import pdb; pdb.set_trace()
+        assert np.allclose(optimal_image.spectrum['uncertainty']**2, box_image.spectrum['uncertainty']**2,0.1)
+
     # TODO write a test which tests that: the optimal extraction weights are correct, and tests that test whether:
     #  1. the variance of the optimal extracted spectrum is the same as the box extracted spectrum in the Poisson noise regime.
     #  2. the variance of the optimal extracted spectrum is ~1.69 times the box extracted spectrum in the read noise regime
@@ -88,6 +124,19 @@ def two_order_image():
     data = np.ones_like(traces, dtype=float)
     data[~np.isclose(traces, 0)] = 100.
     uncertainty = 1. * data
+    image = NRESObservationFrame([EchelleSpectralCCDData(data=data, uncertainty=uncertainty,
+                                                         meta={'OBJECTS': 'tung&tung&none'})], 'foo.fits')
+    image.traces = traces
+    return image
+
+def five_hundred_square_image(maxflux,number_traces,trace_width):
+    traces = np.zeros((500,500))
+    for i in range (0,number_traces): traces[50*i:50*i+trace_width,:]=i
+    data = np.ones_like(traces, dtype=float)
+    data[~np.isclose(traces, 0)] = maxflux
+    data+=np.random.randn(500,500)+10.
+    data+=np.random.poisson(data)
+    uncertainty = 10. + np.sqrt(data)
     image = NRESObservationFrame([EchelleSpectralCCDData(data=data, uncertainty=uncertainty,
                                                          meta={'OBJECTS': 'tung&tung&none'})], 'foo.fits')
     image.traces = traces
