15de5de1de901e8bfa5a769e944daf884b17bc55
gmbrandt <gmbrandt@ucsb.edu>
2020-03-16 13:35:37 -0700

    updated tests for profile fitting.

--- banzai_nres/tests/test_profile.py
+++ banzai_nres/tests/test_profile.py
@@ -7,27 +7,39 @@ from banzai_nres.profile import ProfileFitter
 from banzai import context
 
 
-class TestProfileFitter:
-    def test_fit(self):
-        nx, ny = 401, 403
-        x2d, y2d = np.meshgrid(np.arange(nx), np.arange(ny))
-        test_data, trace_centers, input_traces = make_simple_traces(nx, ny)
-        read_noise = 10.0
+def test_profile_fit_without_noise_without_blaze():
+    nx, ny = 401, 403
+    input_profile, trace_centers, input_traces = make_simple_traces(nx, ny, blaze=False)
+    # Filter out the numerical noise
+    input_profile[input_profile < 1e-15] = 0.0
+    uncertainty = np.sqrt(input_profile)
+    image = NRESObservationFrame([EchelleSpectralCCDData(data=input_profile.copy(), uncertainty=uncertainty,
+                                                         meta={'OBJECTS': 'tung&tung&none'})], 'foo.fits')
+    image.traces = input_traces
+    input_context = context.Context({})
 
-        # Filter out the numerical noise
-        test_data[test_data < 1e-15] = 0.0
-        test_data += np.random.poisson(test_data)
-        test_data += np.random.normal(0.0, read_noise, size=test_data.shape)
-        uncertainty = np.sqrt(test_data + read_noise ** 2.0)
-        image = NRESObservationFrame([EchelleSpectralCCDData(data=test_data, uncertainty=uncertainty,
-                                                             meta={'OBJECTS': 'tung&tung&none'})], 'foo.fits')
-        image.traces = input_traces
-        input_context = context.Context({})
+    stage = ProfileFitter(input_context)
+    image = stage.do_stage(image)
+    scale_factor = np.max(input_profile) / np.max(image.profile)
+    assert np.allclose(input_profile[input_traces != 0], image.profile[input_traces != 0] * scale_factor, rtol=1e-3)
 
-        stage = ProfileFitter(input_context)
-        image = stage.do_stage(image)
 
-        import matplotlib.pyplot as plt
-        plt.imshow(image.profile, vmin=-0.05, vmax=1.0)
-        plt.show()
-        import pdb; pdb.set_trace()
+def test_profile_fit_with_noise_with_blaze():
+    nx, ny = 401, 403
+    input_profile, trace_centers, input_traces = make_simple_traces(nx, ny, blaze=True)
+    read_noise = 10.0
+
+    # Filter out the numerical noise
+    input_profile[input_profile < 1e-15] = 0.0
+    input_profile += np.random.poisson(input_profile)
+    input_profile += np.random.normal(0.0, read_noise, size=input_profile.shape)
+    uncertainty = np.sqrt(input_profile + read_noise ** 2.0)
+    image = NRESObservationFrame([EchelleSpectralCCDData(data=input_profile.copy(), uncertainty=uncertainty,
+                                                         meta={'OBJECTS': 'tung&tung&none'})], 'foo.fits')
+    image.traces = input_traces
+    input_context = context.Context({})
+
+    stage = ProfileFitter(input_context)
+    image = stage.do_stage(image)
+    scale_factor = np.max(image.profile)
+    assert np.allclose(input_profile[input_traces != 0], image.profile[input_traces != 0] * scale_factor, rtol=1e-3)
