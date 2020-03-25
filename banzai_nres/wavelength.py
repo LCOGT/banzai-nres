0146e8be02b3643fef0e993b506d93afa3b4281d
gmbrandt <gmbrandt@ucsb.edu>
2020-03-25 10:56:50 -0700

    broke up feature identification into its own stage.

--- banzai_nres/wavelength.py
+++ banzai_nres/wavelength.py
@@ -17,6 +17,7 @@ MODELS = {'initial_wavelength_model': {1: [0, 1, 2], 2: [0, 1, 2]},
                                      2: [0, 1, 2, 3, 4, 5],
                                      3: [0, 1, 2, 3, 4, 5],
                                      4: [0]}}
+EMISSION_FEATURE_SIZE = 6.0 # pixels
 
 
 class WavelengthLoader(Stage):
@@ -39,26 +40,27 @@ class WavelengthCalibrate(Stage):
     We lightly recalibrate the wavelength calibration if the arc has reference id's assigned to each trace.
     """
     def do_stage(self, image: EchelleSpectralCCDData):
-        # image.spectrum = Table({'id': trace_ids, 'flux': flux, 'uncertainty': np.sqrt(variance)})
-        # identify emission lines and get (pixel, order) positions.
-        features = identify_features(image.data, image.uncertainty, image.mask, nsigma=5.0, fwhm=6.0)
-        features = group_features_by_trace(features, image.traces)
-        features = features[features['id'] != 0]  # throw out features that are outside of any trace.
-        features['flux'] = aperture_extract(features['xcentroid'], features['ycentroid'], image.data, image.mask, aperture_width=6.0)
-        # helps to have flat fielded emission line fluxes.
-        fiber, ref_id, measured_lines, line_list = None, None, None, None
-        pixel, order = np.arange(image.data.shape[1]), np.arange(len(image.spectrum[image.spectrum['fiber'] == fiber]))
-        if ref_id is None:
-            # reidentify reference ids.
-            # blind solve for the wavelengths of the emission lines
-            measured_lines['wavelength'] = wavelength_calibrate(measured_lines, line_list, pixel, order,
-                                                                principle_order_number=NRES_PRINCIPLE_ORDER_NUMBER)
-        else:
-            # adopt wavelengths for the emission lines from the existing solution, good to 1 pixel.
-            # Note: we could improve this considerably with ndimage.map_coordinates(..., order=1, prefilter=False).
-            measured_lines['wavelength'] = image.wavelengths[measured_lines['order'].astype(int),
-                                                             measured_lines['pixel'].astype(int)]
-        wavelength_solution = self.recalibrate(measured_lines, line_list, pixel, order)
+        # load ref_id, id, fibers from Arc lamp
+        ref_id = None
+        id = None
+        fibers = None
+        for fiber in list(set(fibers)):
+            pixel, order = np.arange(image.data.shape[1]), np.sort(ref_id[fibers == fiber])
+            if ref_id is None:
+                # reidentify reference ids.
+                # blind solve for the wavelengths of the emission lines
+                measured_lines['wavelength'] = wavelength_calibrate(measured_lines, line_list, pixel, order,
+                                                                    principle_order_number=NRES_PRINCIPLE_ORDER_NUMBER)
+            else:
+                # assign each feature the correct reference id (order) and fiber:
+                image.features['order'], image.features['fiber'] = ref_id[image.features['id'] - 1], fibers[
+                    image.features['id'] - 1]
+                # adopt wavelengths for the emission lines from the existing solution, good to 1 pixel.
+                # Note: we could improve this considerably with ndimage.map_coordinates(..., order=1, prefilter=False).
+                measured_lines['wavelength'] = image.wavelengths[measured_lines['order'].astype(int),
+                                                                 measured_lines['pixel'].astype(int)]
+            wavelength_solution = self.recalibrate(measured_lines, line_list, pixel, order)
+        # evaluate wavelenlgth_solution at every point in the trace
         # do some qc checks. Throw warnings when necessary.
         return image
 
@@ -86,3 +88,21 @@ class WavelengthCalibrate(Stage):
         weights[np.isclose(wavelengths_to_fit, measured_lines['wavelength'], atol=0.1)] = 1
         wavelength_solution.model_coefficients = wavelength_solution.solve(measured_lines, wavelengths_to_fit, weights=weights)
         return wavelength_solution
+
+
+class IdentifyFeatures(Stage):
+    """
+    Stage to identify all sharp emission-like features on an Arc lamp frame.
+    """
+    def do_stage(self, image: EchelleSpectralCCDData):
+        # identify emission feature (pixel, order) positions.
+        features = identify_features(image.data, image.uncertainty, image.mask, nsigma=5.0, fwhm=EMISSION_FEATURE_SIZE)
+        features = group_features_by_trace(features, image.traces)
+        features = features[features['id'] != 0]  # throw out features that are outside of any trace.
+        # get total flux in each emission feature
+        features['flux'] = aperture_extract(features['xcentroid'], features['ycentroid'], image.data,
+                                            aperture_width=EMISSION_FEATURE_SIZE, mask=image.mask)
+        # blaze correct the emission features fluxes. This speeds up and improves overlap fitting in xwavecal.
+        features['corrected_flux'] = features['flux'] / image.blaze['blaze'][features['id'],
+                                                                             np.array(features['xcentroid'], dtype=int)]
+        image.features = features
