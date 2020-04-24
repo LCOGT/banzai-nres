cb5cefb149906fd9519d25bd21573e734950a83c
gmbrandt <gmbrandt@ucsb.edu>
2020-04-24 13:27:11 -0700

    added necessary information tot the settings.py for things to stack.

--- banzai_nres/settings.py
+++ banzai_nres/settings.py
@@ -23,12 +23,16 @@ ORDERED_STAGES = [
 
 CALIBRATION_MIN_FRAMES = {'BIAS': 5,
                           'DARK': 3,
-                          'LAMPFLAT': 5}
+                          'LAMPFLAT': 5,
+                          'DOUBLE': 1,
+                          }
 
 CALIBRATION_SET_CRITERIA = {'BIAS': ['binning', 'configuration_mode'],
                             'DARK': ['binning', 'configuration_mode'],
                             'LAMPFLAT': ['binning', 'fiber0_lit', 'fiber1_lit', 'fiber2_lit'],
-                            'LINELIST': []}
+                            'DOUBLE': ['binning', 'fiber0_lit', 'fiber1_lit', 'fiber2_lit'],
+                            'LINELIST': [],
+                            }
 
 CALIBRATION_FILENAME_FUNCTIONS = {'BIAS': ('banzai_nres.utils.file_utils.config_to_filename',
                                            'banzai.utils.file_utils.ccdsum_to_filename'),
@@ -36,11 +40,15 @@ CALIBRATION_FILENAME_FUNCTIONS = {'BIAS': ('banzai_nres.utils.file_utils.config_
                                            'banzai.utils.file_utils.ccdsum_to_filename'),
                                   'LAMPFLAT': ('banzai_nres.utils.file_utils.config_to_filename',
                                                'banzai.utils.file_utils.ccdsum_to_filename',
-                                               'banzai_nres.fibers.fibers_state_to_filename')}
+                                               'banzai_nres.fibers.fibers_state_to_filename'),
+                                  'DOUBLE': ('banzai_nres.utils.file_utils.config_to_filename',
+                                             'banzai.utils.file_utils.ccdsum_to_filename',
+                                             'banzai_nres.fibers.fibers_state_to_filename')
+                                  }
 
 TELESCOPE_FILENAME_FUNCTION = 'banzai_nres.utils.runtime_utils.get_telescope_filename'
 
-CALIBRATION_IMAGE_TYPES = ['BPM', 'BIAS', 'DARK', 'LAMPFLAT', 'ARC']
+CALIBRATION_IMAGE_TYPES = ['BPM', 'BIAS', 'DARK', 'LAMPFLAT', 'DOUBLE']
 
 LAST_STAGE = {'BIAS': 'banzai.trim.Trimmer',
               'DARK': 'banzai.bias.BiasSubtractor',
@@ -79,7 +87,9 @@ CALIBRATION_STACKER_STAGES = {'BIAS': ['banzai.bias.BiasMaker'],
 # Stack delays are expressed in seconds--namely, each is five minutes
 CALIBRATION_STACK_DELAYS = {'BIAS': 300,
                             'DARK': 300,
-                            'LAMPFLAT': 300}
+                            'LAMPFLAT': 300,
+                            'DOUBLE': 300,
+                            }
 
 SCHEDULE_STACKING_CRON_ENTRIES = {'cpt': {'minute': 0, 'hour': 15},
                                   'tlv': {'minute': 45, 'hour': 8},
@@ -97,7 +107,8 @@ OBSERVATION_REQUEST_TYPES = {'BIAS': 'NRESBIAS', 'DARK': 'NRESDARK'}
 # For some extension names, we want to just have corresponding BPM or ERR extensions
 EXTENSION_NAMES_TO_CONDENSE = ['SPECTRUM']
 
-CALIBRATION_LOOKBACK = {'BIAS': 4.5, 'DARK': 4.5, 'LAMPFLAT': 0.5}
+# number of days to lookback and stack frames:
+CALIBRATION_LOOKBACK = {'BIAS': 4.5, 'DARK': 4.5, 'LAMPFLAT': 0.5, 'DOUBLE': 0.5}
 
 PIPELINE_VERSION = banzai_nres.__version__
 
