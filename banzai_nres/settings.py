e25fb6430614d3cbeb9d3dbae038be42e9bd02fc
gmbrandt <gmbrandt@ucsb.edu>
2020-04-28 15:05:52 -0700

    disabled background subtraction on arcs prior to feature identifying.

--- banzai_nres/settings.py
+++ banzai_nres/settings.py
@@ -77,7 +77,7 @@ CALIBRATION_STACKER_STAGES = {'BIAS': ['banzai.bias.BiasMaker'],
                                          'banzai_nres.flats.FlatLoader',  # load traces
                                          'banzai_nres.wavelength.ArcLoader',  # load wavelengths, ref_ids, etc...
                                          'banzai_nres.wavelength.LineListLoader',  # load reference laboratory wavelengths
-                                         'banzai_nres.background.BackgroundSubtractor',
+                                         #'banzai_nres.background.BackgroundSubtractor',
                                          'banzai_nres.wavelength.IdentifyFeatures',
                                          'banzai_nres.wavelength.WavelengthCalibrate',
                                          #'banzai_nres.qc.AssessWavelengthSolution'
