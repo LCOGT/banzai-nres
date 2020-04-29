2c3fffabf9b04dd7314783415c8c6dc7f5fb4760
Marshall Johnson <mjohnson@lco.global>
2020-04-29 13:20:19 -0700

    Partial cleanup of code

--- banzai_nres/qc/wavelength_solution_checker.py
+++ banzai_nres/qc/wavelength_solution_checker.py
@@ -21,16 +21,15 @@ class AssessWavelengthSolution(Stage):
         lab_lines = find_nearest(image.features['wavelength'], np.sort(line_list))
         self.calculate_delta_lambda(image,lab_lines,Delta_lambda)
         self.calculate_1d_metrics(image,Delta_lambda,sigma_Dlambda,good_sigma_Dlambda,raw_chi_squared,good_chi_squared,)
-        self.calculate_2d_metrics(image,Delta_lambda,x_sigma_Dlambda, order_sigma_Dlambda)
+        self.calculate_2d_metrics(image,Delta_lambda,x_diff_Dlambda, order_diff_Dlambda)
         qc_results = {'sigma_Dlambda':sigma_Dlambda, 'good_sigma_Dlambda':good_sigma_Dlambda, 
                         'raw_chi_squared':raw_chi_squared, 'good_chi_squared':good_chi_squared, 
-                        'Delta_lambda':Delta_lambda, 'x_sigma_Dlambda':x_sigma_Dlambda, 
-                        'order_sigma_Dlambda':order_sigma_Dlambda}
+                        'Delta_lambda':Delta_lambda, 'x_diff_Dlambda':x_diff_Dlambda, 
+                        'order_diff_Dlambda':order_diff_Dlambda}
         qc.save_qc_results(self.runtime_context, qc_results, image)
 
     def calculate_delta_lambda(self,image,lab_lines):
         measured_wavelengths = image.features['wavelength']
-        #actual_wavelengths = image.wcs.measured_lines['wavelength']
         Delta_lambda = measured_wavelengths - lab_lines
         return Delta_lambda
 
@@ -49,12 +48,12 @@ class AssessWavelengthSolution(Stage):
         histogramx, bins_x = np.histogram(x, bins=bins, range=([np.min(x)-1,np.max(x)+1]))
         histogramo, bins_order = np.histogram(order, bins=bins, range=([np.min(order)-1,np.max(order)+1]))
         x_indices, order_indices = np.digitize(x, bins_x[1:]), np.digitize(order, bins_order[1:])
-        x_sigma_Dlambda, order_sigma_Dlambda = np.zeros_like(histogramx), np.zeros_like(histogramo)
+        x_diff_Dlambda, order_diff_Dlambda = np.zeros_like(histogramx), np.zeros_like(histogramo)
         for i in range (0,bins):
-            if histogramx[i] != 0: x_sigma_Dlambda[i] = np.mean(Delta_lambda[x_indices == i])
-            if histogramo[i] != 0: order_sigma_Dlambda[i] = np.mean(Delta_lambda[order_indices == i])
-        return x_sigma_Dlambda, order_sigma_Dlambda
+            if histogramx[i] != 0: x_diff_Dlambda[i] = np.mean(Delta_lambda[x_indices == i])
+            if histogramo[i] != 0: order_diff_Dlambda[i] = np.mean(Delta_lambda[order_indices == i])
+        return x_diff_Dlambda, order_diff_Dlambda
 
 
         #TO DO:
-        #Rename variables, clean up code
+        #clean up code
