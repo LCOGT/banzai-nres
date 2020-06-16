c207b2b0b88e9f3c886e32f34b785b52ddf9c6c4
Curtis McCully <cmccully@lco.global>
2020-06-16 11:40:10 -0400

    Fixed a typo in getting which fiber is which.

--- banzai_nres/wavelength.py
+++ banzai_nres/wavelength.py
@@ -7,12 +7,12 @@ from banzai_nres.frames import NRESObservationFrame
 from banzai_nres.utils.wavelength_utils import identify_features, group_features_by_trace, get_principle_order_number
 from xwavecal.wavelength import find_feature_wavelengths, WavelengthSolution
 from xwavecal.utils.wavelength_utils import find_nearest
-from astropy.table import vstack
 
 import sep
 import logging
 import os
 import pkg_resources
+from astropy.table import Table
 
 
 logger = logging.getLogger('banzai')
@@ -137,12 +137,21 @@ class WavelengthCalibrate(Stage):
                 image.wavelengths[this_trace] = wavelength_model(x2d[this_trace], ref_id * np.ones_like(x2d[this_trace]))
             # Set the true physical order number
             ref_ids[fiber_ids == fiber] += m0
-        # TODO: Add the logic to calculate which fiber is which.
+        # Calculate which fiber is which.
         # the first ref_id = 50 (arbitrary, but somewhere about the middle of the detector)
         # is largest fiber number that is lit.
-        # in output table fiber=fiber set to real fiber that is lit
-        # Other fiber_id is set to other lit fiber
+        largest_fiber_index = fiber_ids[ref_ids.tolist().index(50)]
+        if image.fiber2_lit:
+            largest_fiber_number = 2
+            smaller_fiber_number = 1
+        else:
+            largest_fiber_number = 1
+            smaller_fiber_number = 0
+
+        true_fibers = [largest_fiber_number if fiber_id == largest_fiber_index else smaller_fiber_number
+                       for fiber_id in fiber_ids]
         # set fibers attribute on image
+        image.fibers = Table({'trace': trace_ids, 'fiber': true_fibers})
 
     @staticmethod
     def init_feature_labels(num_traces, features):
