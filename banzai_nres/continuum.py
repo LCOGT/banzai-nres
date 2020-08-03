91e4dd36e077ca70c1e862d0bfa5bef295dd7816
gmbrandt <gmbrandt@ucsb.edu>
2020-08-03 14:10:00 -0700

    better continuum subtraction based on finite differences.

--- banzai_nres/continuum.py
+++ banzai_nres/continuum.py
@@ -4,31 +4,38 @@ from banzai_nres.fitting import fit_polynomial
 import numpy as np
 
 from scipy.ndimage.morphology import binary_dilation
-from scipy.signal import find_peaks
+from scipy.stats import median_absolute_deviation
+
+# Wavelength regions where there are strong Balmer or other absorption lines. This is from CERES (thanks!)
+WAVELENGTHS_TO_MASK = np.array([[6755, 6769], [6530, 6600], [4840, 4880], [4320, 4360],
+                                [4085, 4120], [3950, 3990], [3880, 3910], [3825, 3850]])
 
 
 class ContinuumNormalizer(Stage):
     def do_stage(self, image) -> NRESObservationFrame:
         for fiber, order in zip(*image.spectrum.fibers_and_orders):
             spectrum = image.spectrum[fiber, order]
-            normalized_flux = spectrum['flux'] / spectrum['blaze']
-            normalized_error = normalized_flux * np.sqrt((spectrum['uncertainty'] / spectrum['flux']) ** 2.0 + (spectrum['blaze_error'] / spectrum['blaze']) ** 2.0)
-            # identify absorption features. negative of Normalized flux runs from -1 to 0 ideally. Deepest lines at -1.
+            blaze_corrected_flux = spectrum['flux'] / spectrum['blaze']
+            blaze_corrected_error = blaze_corrected_flux * np.sqrt((spectrum['uncertainty'] / spectrum['flux']) ** 2.0 + (spectrum['blaze_error'] / spectrum['blaze']) ** 2.0)
             detector_resolution = 4  # pixels
-            peaks, properties = find_peaks(-1*normalized_flux, height=-0.75, distance=2*detector_resolution,
-                                           prominence=0.2, width=[detector_resolution, 200])
-            # TODO use signal.peak_widths (or maybe find_peaks has these in properties) to
-            #  mask pixels later on using the peak widths, not the detector resolution.
-            # Mask absorption features
-            mask = np.zeros_like(normalized_flux, dtype=int)
-            mask[peaks] = 1
-            # Mask the immediate +- two pixels around each absorption feature as well.
-            mask = binary_dilation(mask, iterations=int(detector_resolution/2))
+            # identify features quickly
+            first_derivative = np.hstack([[0], blaze_corrected_flux[1:] - blaze_corrected_flux[:-1]])
+            mad = median_absolute_deviation(first_derivative)
+            features = np.abs(first_derivative) > 1.5 * mad
+            # Mask features
+            mask = np.zeros_like(blaze_corrected_flux, dtype=int)
+            mask[features] = 1
+            # Mask the prohibited wavelength regions.
+            for mask_region in WAVELENGTHS_TO_MASK:
+                mask[np.logical_and(spectrum['wavelength'] >= min(mask_region), spectrum['wavelength'] <= max(mask_region))] = 1
+            # Mask the immediate +- pixels around each absorption feature as well.
+            # 2 binary dilations work too, but we will binary dilate up to the detector resolution.
+            mask = binary_dilation(mask, iterations=int(detector_resolution))
             # do the fit.
-            best_fit = fit_polynomial(normalized_flux, normalized_error, x=spectrum['wavelength'],
-                                      order=3, sigma=2, mask=mask)
-            image.spectrum[fiber, order, 'normflux'] = normalized_flux / best_fit(spectrum['wavelength'])
+            best_fit = fit_polynomial(blaze_corrected_flux, blaze_corrected_error, x=spectrum['wavelength'],
+                                      order=3, sigma=5, mask=mask)
+            image.spectrum[fiber, order, 'normflux'] = blaze_corrected_flux / best_fit(spectrum['wavelength'])
             # Technically, this should take into account the fit uncertainty using e.g. MCMC but the covariance it
             # adds is complicated enough to track that we do not attempt that here.
-            image.spectrum[fiber, order, 'normuncertainty'] = normalized_error / best_fit(spectrum['wavelength'])
+            image.spectrum[fiber, order, 'normuncertainty'] = blaze_corrected_error / best_fit(spectrum['wavelength'])
         return image
