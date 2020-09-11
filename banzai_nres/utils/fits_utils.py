7891553d088d1d7e0fe31402d180b983cba098b5
Curtis McCully <cmccully@lco.global>
2020-09-11 15:19:07 -0400

    Fixes to byte strings in loading phoenix models into the db.

--- banzai_nres/utils/fits_utils.py
+++ banzai_nres/utils/fits_utils.py
@@ -6,7 +6,7 @@ def fits_bytes_to_header(header_lines):
     for i in range(0, len(header_lines), 80):
         header_line = header_lines[i:i+80]
         keyword = header_line[:8].strip()
-        comment_start = header_line.find("\\")
+        comment_start = header_line.find(b"\\")
         value = header_line[9:comment_start].strip()
         try:
             value = int(value)
@@ -15,9 +15,9 @@ def fits_bytes_to_header(header_lines):
                 value = float(value)
             except:
                 pass
-        if value == 'T':
+        if value == b'T':
             value = True
-        elif value == 'F':
+        elif value == b'F':
             value = False
         comment = header_line[comment_start + 1:].strip()
 
