67ad5c39c1e45e6ed10aed2729f9b896c0c4a7bc
Curtis McCully <cmccully@lco.global>
2021-01-06 15:08:52 -0500

    Bugfix in substamp.

--- banzai_nres/traces.py
+++ banzai_nres/traces.py
@@ -48,7 +48,7 @@ def refine_traces(image, weights=None):
         logger.info(f'Fitting a polynomial to order {i}', image=image)
         # we chose order 5 based on visually inspecting the residuals between the trace centers and the model fit centers
         # TODO we need to verify that an order 5 polynomial fit is the best thing to do.
-        best_fit = fit_polynomial(y_center[x_center.astype(int)], y_center_errors[x_center.astype(int)], x=x_center, order=5)
+        best_fit = fit_polynomial(y_center, y_center_errors, x=x_center, order=5)
         # TODO: extrapolate fit and go out to pixels that have a cumulative S/N = 10
         y_center = best_fit(x_center)
 
