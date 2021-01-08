c154dec55afcc44073168dac62b62458ce1b9864
Curtis McCully <cmccully@lco.global>
2021-01-08 10:48:48 -0500

    Actually mock the right function calls. There is a weird import issue.

--- banzai_nres/tests/test_e2e.py
+++ banzai_nres/tests/test_e2e.py
@@ -19,7 +19,6 @@ from astropy.table import Table
 from banzai.utils import fits_utils
 import banzai_nres.dbs
 import json
-import banzai_nres.dbs
 
 import logging
 
@@ -42,11 +41,23 @@ PHOENIX_FILENAME = pkg_resources.resource_filename('banzai_nres.tests', 'data/ph
 
 
 class MockGaiaCatalog:
-    pass
+    def query_object(self, *args, **kwargs):
+        return []
 
 
 class MockSimbad:
-    pass
+    def add_votable_fields(self, *args, **kwargs):
+        pass
+
+    def query_region(self, *args, **kwarg):
+        data = {'RA': ['07 39 18.1195', '07 39 17.8800'],
+                'DEC': ['+05 13 29.955', '+05 13 26.800'],
+                'PMRA': [-714.59,  -709.],
+                'PMDEC': [-1036.8, -1024.],
+                'Fe_H_Teff': [6474, 7870],
+                'Fe_H_log_g': [3.9642, 8.08],
+                'Fe_H_Fe_H': [0.01, np.nan]}
+        return Table(data)
 
 
 def observation_portal_side_effect(*args, **kwargs):
