b8c0e9fefd6e720aa927964c29402c78aba1c2ea
Curtis McCully <cmccully@lco.global>
2021-03-04 18:40:25 -0500

    Removed stuff from the init.py that I don't think ever worked.

--- banzai_nres/__init__.py
+++ banzai_nres/__init__.py
@@ -1,11 +1,4 @@
-from pkg_resources import get_distribution, DistributionNotFound
 import logging
 from banzai.logs import BanzaiLogger
-
-try:
-    __version__ = get_distribution('lco-banzai-nres-e2e').version
-except DistributionNotFound:
-    # package is not installed
-    pass
 
 logging.setLoggerClass(BanzaiLogger)
