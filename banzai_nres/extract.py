048894049ca10dae02b61404c3fa935bae8baf2b
Matt Daily <matt.daily85@gmail.com>
2021-03-08 16:43:48 -0800

    PEP8 Fixes to classify, extract, frames and settings modules

--- banzai_nres/extract.py
+++ banzai_nres/extract.py
@@ -28,9 +28,11 @@ class WeightedExtract(Stage):
             # get the horizontal (x) extent of the trace. Consider making this a get_extent function.
             x_extent = slice(np.min(this_trace[1]), np.max(this_trace[1]) + 1)
             flux[i, x_extent] = self.extract_order(image.data[this_trace], image.weights[this_trace])
-            variance[i, x_extent] = self.extract_order(image.uncertainty[this_trace] ** 2, image.weights[this_trace] ** 2)
+            variance[i, x_extent] = self.extract_order(image.uncertainty[this_trace] ** 2,
+                                                       image.weights[this_trace] ** 2)
             # get the average wavelength: Sum wavelengths weighted by 1 over the vertical width of the trace (e.g. 1/10)
-            wavelength[i, x_extent] = self.extract_order(image.wavelengths[this_trace], weights=1/image.wavelengths[this_trace].shape[0])
+            wavelength[i, x_extent] = self.extract_order(image.wavelengths[this_trace],
+                                                         weights=1/image.wavelengths[this_trace].shape[0])
             mask[i, x_extent] = image.weights[this_trace].sum(axis=0) == 0.0
 
         image.spectrum = Spectrum1D({'id': trace_ids, 'order': image.fibers['order'],
