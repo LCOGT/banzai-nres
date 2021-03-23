d0b48dd63b43d287ac48ed7fe9fc2e6ef89bcfdb
Curtis McCully <cmccully@lco.global>
2021-03-23 13:13:48 -0400

    Attempt to have eager loading for the relationships between classfications and models.

--- banzai_nres/dbs.py
+++ banzai_nres/dbs.py
@@ -104,7 +104,7 @@ class Classification(Base):
     sin_dec = Column(Float)
     cos_dec = Column(Float)
     phoenix_id = Column(Integer, ForeignKey('phoenixmodels.id'))
-    phoenix_model = relationship("PhoenixModel", lazy='joined')
+    phoenix_model = relationship("PhoenixModel", lazy='subquery')
     Index('idx_radec', "ra", "dec")
     Index('idx_radectrig', "sin_ra", "cos_ra", "sin_dec", "cos_dec")
 
