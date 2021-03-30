0bdc1c40967aafc72d2cb06c24a4b3b9ac48c971
captain-exoplanet <mjohnson@lco.global>
2021-03-30 10:20:32 -0700

    Codestyle changes

--- banzai_nres/qc/qc_science.py
+++ banzai_nres/qc/qc_science.py
@@ -1,13 +1,12 @@
 import numpy as np
 from banzai.stages import Stage
-from banzai.utils import qc
 import logging
 
 logger = logging.getLogger('banzai')
 
-#This defines the order for which we calculate the SNR value for the header.
-#By default this is the order containing the Mg b lines.
-SNR_ORDER = 90 
+# This defines the order for which we calculate the SNR value for the header.
+# By default this is the order containing the Mg b lines.
+SNR_ORDER = 90
 
 
 class CalculateScienceFrameMetrics(Stage):
@@ -21,7 +20,8 @@ class CalculateScienceFrameMetrics(Stage):
 
         return image
 
+
 def get_snr(image, order):
-    snr_all = image.spectrum[image.science_fiber,order]['flux'] / image.spectrum[image.science_fiber,order]['uncertainty']
-    snr = np.percentile(snr_all,90)
+    snr_all = image.spectrum[image.science_fiber, order]['flux'] / image.spectrum[image.science_fiber, order]['uncertainty']
+    snr = np.percentile(snr_all, 90)
     return snr
