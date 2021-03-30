ae4b88e381a0a32f5cdadcf0bda4efad8a6a8cf8
captain-exoplanet <mjohnson@lco.global>
2021-03-30 07:55:30 -0700

    qc_science tests now work

--- banzai_nres/tests/test_qc_science.py
+++ banzai_nres/tests/test_qc_science.py
@@ -1,19 +1,24 @@
 import numpy as np
-
+from astropy.io import fits
 from banzai import context
-from banzai.nres.qc.qc_science import CalculateScienceFrameMetrics
-from banzai.nres.qc.qc_science import get_snr
+from banzai_nres.qc.qc_science import CalculateScienceFrameMetrics
+from banzai_nres.qc.qc_science import get_snr
 from banzai_nres.frames import NRESObservationFrame
 from banzai_nres.frames import EchelleSpectralCCDData, Spectrum1D
 
 class TestCalculateScienceFrameMetrics:
-    nput_context = context.Context({})
+    input_context = context.Context({})
     test_wavelengths = np.linspace(5100.0,5200.0,4096)
     test_flux =  -0.001 * test_wavelengths**2 + 10.3 * test_wavelengths
     test_uncertainty = np.sqrt(test_flux)
     snr_order = 90
-    spectrum = {'wavelength': test_wavelengths, 'flux': test_flux, 'uncertainty': test_uncertainty, 'fiber': 0, 'order': snr_order}
-    image = NRESObservationFrame([EchelleSpectralCCDData(np.zeros((1, 1)), spectrum=Spectrum1D(spectrum))], science_fiber=0,
+    spectrum = []
+    header = fits.Header({'OBJECTS': 'test&none&none'})
+    for i in range(5):
+        order = snr_order + i
+        row = {'wavelength': test_wavelengths, 'flux': test_flux, 'uncertainty': test_uncertainty, 'fiber': 0, 'order': snr_order}
+        spectrum.append(row)
+    test_image = NRESObservationFrame([EchelleSpectralCCDData(np.zeros((1, 1)), meta=header, spectrum=Spectrum1D(spectrum))],
                                  'test.fits')
     
 
@@ -22,5 +27,5 @@ class TestCalculateScienceFrameMetrics:
         assert image is not None
 
     def test_snr_calculation(self):
-        snr = get_snr(image,snr_order)
-        assert np.isclose(snr, np.max(test_flux/test_uncertainty), rtol=0.1)
+        snr = get_snr(self.test_image,self.snr_order)
+        assert np.isclose(snr, np.max(self.test_flux/self.test_uncertainty), rtol=0.1)
